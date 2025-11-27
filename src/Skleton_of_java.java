cf65ba9c9f8260e366e7da273b677c1b92336476
sahil0sharma <sahil0sharma5647@gmail.com>
2025-11-27 19:56:14 +0530

    Introduction and skleton of java programs

---
+++ src/Skleton_of_java.java
@@ -0,0 +1,12 @@
+
+
+class Skleton_of_java {                                         //calss declared
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+//      public --> it  means this method can be access outside of calss
+//      static --> this method is part of calss itself rather than an instance of calss
+//      void   --> there is no return type of this method
+//      main   --> name of class
+//      String args --> aceept parameter as string
