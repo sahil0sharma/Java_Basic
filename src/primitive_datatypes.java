c00dac870c721eee0889ab05debbfaa179f585d7
sahil0sharma <sahil0sharma5647@gmail.com>
2025-11-27 22:03:09 +0530

    Introduction and skleton of java programs

---
+++ src/primitive_datatypes.java
@@ -0,0 +1,48 @@
+//Primitive datatype :- bilt-in data types that cannot be use as variable names
+public class primitive_datatypes {
+    public static void main(String[] args) {
+
+        //NUMBERS
+
+        int a = 10;
+        System.out.println(a);
+        System.out.println( Integer.MIN_VALUE);
+        System.out.println( Integer.MAX_VALUE);
+
+        //DECIMAL VALUE
+
+        float b = 56.7f;     //less precision needf
+        System.out.println(b);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        double c = 10.00000000000000000;
+        System.out.println(c);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        // CHARACTER
+
+        char d = 'x';       // in single course
+        System.out.println(d);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        // type cast to number
+        System.out.println( (int) d );
+        //
+        char x = 120;
+        System.out.println(x);
+
+        // BOOLEAN
+
+        boolean e = true;
+        System.out.println(e);
+
+
+
+
+
+
+
+    }
+}
