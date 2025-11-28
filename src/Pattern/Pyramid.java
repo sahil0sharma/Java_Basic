e979fe5c51872b3d5f08cdc1f82b1519bb34e216
sahil0sharma <sahil0sharma5647@gmail.com>
2025-11-28 17:34:37 +0530

    Pyramid

---
+++ src/Pattern/Pyramid.java
@@ -0,0 +1,28 @@
+package Pattern;
+
+//    *
+//   ***
+//  *****
+// *******
+//*********
+import java.util.Scanner;
+public class Pyramid {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of rows ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+
+            for (int s = 1; s <= n - i; s++){
+                System.out.print(" ");
+            }
+            for ( int j = 1; j <= 2 * i -1; j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
