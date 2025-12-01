36e90d9f35afe91f06bfca8f473795bfdc4fc548
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-01 21:12:09 +0530

    Class

---
+++ src/Class.java
@@ -0,0 +1,20 @@
+
+class man{
+ String name;
+ int age;
+ float height;
+
+}
+
+public class Class {
+    public static void main(String[] args) {
+
+        man m = new man();
+        m.name = "sahil";
+        m.age = 19;
+        m.height = 5.10f;
+
+        System.out.println(m.name + " " + m.age + " " +  m.height);
+
+    }
+}
