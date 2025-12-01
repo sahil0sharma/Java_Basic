462308296bafb356bc38eba1bbe1ba381930d3bb
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-01 14:39:05 +0530

    Loop

---
+++ src/For_Each_Loop.java
@@ -0,0 +1,19 @@
+public class For_Each_Loop {
+    public static void main(String[] args){
+
+        int[] arr = new int[10];
+
+        for (int i : arr){
+            System.out.println(i);
+        }
+
+        //this is for each loop | int represent type of loop
+        // i represent the local variable that represent the value of org array
+        // arr means which array is talking about
+        // only works for array
+
+
+
+
+    }
+}
